dbf301480014cc327bf4e574de3c71048d1f424a
Oncu <oohancan@gmail.com>
2022-10-19 17:06:43 +0300

    Initial Commit

---
+++ BabySortingToyGames/BabySortingToyGamesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BabySortingToyGamesApp.swift
+//  BabySortingToyGames
+//
+//  Created by Oncu Can on 19.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct BabySortingToyGamesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
